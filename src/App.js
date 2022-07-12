eb47379210f63768567bc059bbf0cf3cf2fdc263
Axel Andree Perez Reyes <105800687+BlackIcee1234@users.noreply.github.com>
2022-07-12 13:23:59 -0500

    Nuevo Proyecto 12/07/2022

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
+import Home from './routes/home/home.component';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 
-import CardList from './components/card-list/card-list.component';
-import './App.css';
-import SearchBox from './components/search-box/search-box.component';
-import { useState, useEffect } from 'react';
+const Shop = () => {
+  return(<h1>I am the Shop Page</h1>);
+};
 
 const App = () => {
-  const [searchField, setSearchField] = useState('');
-  const [monsters, setMonsters] = useState([]);
-  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
-
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => setMonsters(users));
-  },[]);
-
-  useEffect(()=>{
-    const newfilteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-  });
-    setFilteredMonsters(newfilteredMonsters);
-  },[monsters, searchField]);
-
-  const onSearchChange = (event) => {
-        const searchFieldString = event.target.value.toLocaleLowerCase();
-        setSearchField(searchFieldString);
-      };
-  
   return (
-    <div className="App">
-      <h1 className='app-title'>Monsters Rolodex</h1>
-      <SearchBox 
-        onSearchHandler={onSearchChange} 
-        placeholder='Search Monsters' 
-        className='monsters-search-box'/>
-      <CardList monsters={filteredMonsters}/>
-    </div>
+  <Routes>
+    <Route path='/' element={<Navigation/>}>
+      <Route index element={<Home/>}/>
+      <Route path='shop' element={<Shop/>}/>
+    </Route>
+  </Routes>
   );
 };
 
