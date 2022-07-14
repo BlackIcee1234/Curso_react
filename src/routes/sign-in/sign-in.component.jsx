1e98a1b5e7ff53bee11889e3d79a51e24bf15231
Axel Andree Perez Reyes <105800687+BlackIcee1234@users.noreply.github.com>
2022-07-14 11:48:04 -0500

    FORMULARIO SIGN UP

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,31 @@
-import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils';
+import {auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect} from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult} from 'firebase/auth';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
 
+    useEffect(() =>{
+        const redirect = async() =>{
+            const response = await getRedirectResult(auth);
+            if(response){
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+        }
+        redirect();
+    }, []);
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user);
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
 
     return(
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button>
+            <SignUpForm/>
         </div>
     );
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,44 +1,66 @@
-import { initializeApp } from 'firebase/app';
-import { 
-    getAuth, 
-    signInWithRedirect, 
-    signInWithPopup, 
-    GoogleAuthProvider
-} from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider
+} from "firebase/auth";
 
-import{
+import {
   getFirestore,
   doc,
   getDoc,
   setDoc,
-  Firestore
-} from 'firebase/firestore';
+} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCiu_odFrofM0vRzfIwCoRyebaeQaqOVNU",
-    authDomain: "crwn-clothing-axel-db.firebaseapp.com",
-    projectId: "crwn-clothing-axel-db",
-    storageBucket: "crwn-clothing-axel-db.appspot.com",
-    messagingSenderId: "1095663758276",
-    appId: "1:1095663758276:web:939b0fc4681dcf4aa6d291"
-  };
-  
-  const firebaseApp = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCiu_odFrofM0vRzfIwCoRyebaeQaqOVNU",
+  authDomain: "crwn-clothing-axel-db.firebaseapp.com",
+  projectId: "crwn-clothing-axel-db",
+  storageBucket: "crwn-clothing-axel-db.appspot.com",
+  messagingSenderId: "1095663758276",
+  appId: "1:1095663758276:web:939b0fc4681dcf4aa6d291",
+};
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
-    prompt: "select_account"
-  });
+initializeApp(firebaseConfig);
 
-  export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-  
-  export const db = getFirestore();
+const googleProvider = new GoogleAuthProvider();
 
-  export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = doc(db, 'users', userAuth.uid);
-    console.log(userDocRef);
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
 
-    const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot);
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => 
+signInWithPopup(auth, googleProvider);
+
+export const signInWithGoogleRedirect = () => 
+signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
+    }
   }
+  return userDocRef;
+};
