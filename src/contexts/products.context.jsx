030b225654ff1409ffc737f26390fa761e521094
Axel Andree Perez Reyes <105800687+BlackIcee1234@users.noreply.github.com>
2022-07-19 12:06:40 -0500

    19/07/2022 Creacion del carrito

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState} from "react";
 import PRODUCTS from '../shop-data.json';
 
-export const ProductContext = createContext({
+export const ProductsContext = createContext({
     products: [],
 });
 
@@ -9,6 +9,6 @@ export const ProductsProvider = ({children}) => {
     const [products, setProducts] = useState(PRODUCTS);
     const value = {products};
     return (
-        <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
+        <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
     );
 }
