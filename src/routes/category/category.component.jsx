5a18af9a8ff783d55c7082b09e0f375914e84c8b
Axel Andree Perez Reyes <105800687+BlackIcee1234@users.noreply.github.com>
2022-08-09 12:07:08 -0500

    COMMIT DIA 09/08/2022 TERMINACION DE GRAPHQL

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,23 +1,51 @@
 import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
-
 import ProductCard from '../../components/product-card/product-card.component';
-
 import { CategoriesContext } from '../../contexts/categories.context';
-
 import { CategoryContainer, Title } from './category.styles';
+import Spinner from '../../components/spinner/spinner.component';
+import { useQuery, gql } from '@apollo/client';
+
+
+const GET_CATEGORY = gql`
+query($title: String!){
+  getCollectionsByTitle(title: $title){
+    id
+    title
+    items{
+      id
+      name
+      price
+      imageUrl
+    }
+  }
+}
+`;
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  const { loading, error, data} = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category
+    }
+  });
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if(data){
+      const {
+      getCollectionsByTitle: {items}
+      } = data;
+      setProducts(items);
+    }
+  }, [category, data]);
+  const [products, setProducts] = useState([]);
 
   return (
     <Fragment>
+      { 
+      loading ? <Spinner/> :
+      (
+      <Fragment>
       <Title>{category.toUpperCase()}</Title>
       <CategoryContainer>
         {products &&
@@ -25,6 +53,8 @@ const Category = () => {
             <ProductCard key={product.id} product={product} />
           ))}
       </CategoryContainer>
+      </Fragment>)
+      }
     </Fragment>
   );
 };
